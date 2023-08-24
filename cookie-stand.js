cdba6d5936397b82d4f7d3d72952af1684e0d1f8
Caleb-McKinney <McKinney.Caleb@gmail.com>
2023-08-23 21:25:22 -0500

    corrected semantics in code

--- cookie-stand.js
+++ cookie-stand.js
@@ -1,5 +1,5 @@
-'use strict'
- 
+'use strict';
+
 const hours = [
   "6am",
   "7am",
@@ -16,42 +16,47 @@ const hours = [
   "6pm",
   "7pm",
   "8pm"
-]
+];
 
 const Seattle = {
   location: 'Seattle',
   lowCust: 23,
   highCust: 65,
   cookiesPerCust: 6.3,
-}
-
-custPerHour: [],
-
-cookiesPerHour: [],
-totalDaily: 0,
-
-getCustPerHour: function() {
-  for (let i =0; i < hours.length; i++) {
-    const randomCust= getRandomInt(this.minCust, this.maxCust)
-
-    this.custPerHour.push(randomCust)
+  custPerHour: [],
+  cookiesPerHour: [],
+  totalDaily: 0,
+  getCustPerHour: function() {
+    for (let i = 0; i < hours.length; i++) {
+      const randomCust = getRandomInt(this.lowCust, this.highCust);
+      this.custPerHour.push(randomCust);
+    }
+  },
+  countCookies: function() {
+    for (let i = 0; i < this.custPerHour.length; i++) {
+      const soldCookies = this.custPerHour[i] * this.cookiesPerCust;
+      this.cookiesPerHour.push(soldCookies);
+      this.totalDaily += soldCookies;
+    }
+  },
+  render: function() {
+    console.log("render method called");
+    let seattleList = document.getElementById("seattle-list");
+    for (let i = 0; i < hours.length; i++) {
+      let hour = hours[i];
+      let listItem = document.createElement('li');
+      listItem.textContent = `${hour}: ${this.cookiesPerHour[i]} cookies`;
+      seattleList.appendChild(listItem);
+    }
   }
-},
+};
 
-countCookies: function() {
-  for (let i=0; i < custPerHour.length; i++) {
-    const soldCookies = custPerHour[i] * this.cookiesPerCust
-
-    this.cookiesPerHour.push(soldCookies)
-
-    this.totalDaily += soldCookies
-  }
-}
-
-seattle.countCookies();
-seattle.getCookiesPerHour();
+Seattle.getCustPerHour();
+Seattle.countCookies();
+Seattle.render();
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
